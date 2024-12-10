b4763904c90f5eccadf5f11b4410c620163941ca
Mirza Arnaut <mirza.arnaut45@gmail.com>
2024-12-10 13:49:45 +0100

    Temporary skip the cli test locally

--- tests/test__cli.py
+++ tests/test__cli.py
@@ -1,11 +1,13 @@
 from pathlib import Path
 
+import pytest
 from click.testing import CliRunner
 
 from refidxdb import databases
 from refidxdb.cli import cli
 
 
+# @pytest.mark.skip(reason="Takes too many resources for now")
 def test_sync():
     runner = CliRunner()
     result = runner.invoke(cli, ["db", "--download", "all"])
